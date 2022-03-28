2ede136fdcb322c0e9cb6be6b1d5d386c288b80d
billgadov3k <billgadov3k@gmail.com>
2022-03-28 07:30:58 +0300

    add buttons

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const { Telegraf, Markup } = require('telegraf')
-
 const bot = new Telegraf('5120141410:AAGP29nhiwlRus-Nz3b8Z5V-eo3Fz8ko_cE')
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('Стикер'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
-bot.on('text', (ctx) => {
-  console.log(ctx.message)
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Копирую ${ctx.message.text}`)})
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.command('play', async (ctx) => {
+  ctx.reply('Вы готовы учавствовать в испытании?', Markup.inlineKeyboard([
+    [Markup.button.callback('Да', 'yes')],
+    [Markup.button.callback('Нет','no')]
+  ]))
+
+})
+
+bot.action('yes', (ctx) => {
+  ctx.reply('Отлично! Тогда еще пару вопросов перед началом')
+})
+
+bot.action('no', (ctx) => {
+  ctx.reply('Нужно еще прокачаться )))')
+})
 
 bot.launch()
 
